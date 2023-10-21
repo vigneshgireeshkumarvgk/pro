ae50cc24fb2cb6cbfee5a97183b0cdf8c0d0eb54
VIGNESH GIREESHKUMAR <133968003+vigneshgireeshkumarvgk@users.noreply.github.com>
2023-10-21 15:16:04 +0530

    Update rust.sh

--- rust.sh
+++ rust.sh
@@ -1,4 +1,3 @@
-RUSTFLAGS="-C target-feature=-crt-static"
-git clone https://github.com/helix-editor/helix
-cd helix
-cargo install --path helix-term --locked
+git clone https://github.com/dylanaraps/neofetch/releases/latest
+git clone https://github.com/ranger/ranger
+
